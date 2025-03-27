a9fdbb384a7a0376761a8a5e5d93c9bf1f98b44e
EliasBaroudi <153591577+EliasBaroudi@users.noreply.github.com>
2025-03-27 15:50:42 +0100

    Add files via upload

---
+++ PASSWD/PASSWD/GestionnaireViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GestionnaireViewController.swift
+//  PASSWD
+//
+//  Created by Elias Baroudi on 19/03/2025.
+//
+
+import UIKit
+
+class GestionnaireViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
